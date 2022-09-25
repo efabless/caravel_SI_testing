7c67c45c91cf7d303221efb44cbcc71bdb16c874
M0stafaRady <mostafa.rady@efabless.com>
2022-09-25 10:18:11 -0700

    fix recieve_packet order

--- caravel_board/firmware_vex/silicon_tests/common/recieve_packet.c
+++ caravel_board/firmware_vex/silicon_tests/common/recieve_packet.c
@@ -4,20 +4,26 @@
 int recieve_packet(int timeout){
     int packet_size = 0;
     bool is_full_pulse = false; // indecator for full pulse if half pulse is just sent it will be false
+    reg_mprj_datal = 0x0;
     while (reg_gpio_in == 0);
+    reg_mprj_datal = 0x4000;
     while (reg_gpio_in == 1);
+    reg_mprj_datal = 0x0;
     while (reg_gpio_in == 0);
+    reg_mprj_datal = 0x4000;
     while (reg_gpio_in == 1);
     packet_size++; // First packet not expected to get end of transmission 
     is_full_pulse == true;
     int count = 0;
     while (true){
         if (reg_gpio_in == 0){
+            reg_mprj_datal = 0x0;
             count++;
             if (count >= timeout)
             return packet_size;
         }
         else if (reg_gpio_in == 1){
+            reg_mprj_datal = 0x4000;
             count = 0;
             is_full_pulse = !is_full_pulse;
             if (is_full_pulse)

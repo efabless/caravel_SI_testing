7e777973cf0e43cd57c8e156eb5d8a80c0ec8519
M0stafaRady <mostafa.rady@efabless.com>
2022-09-27 08:58:55 -0700

    add config_io_i_low

--- caravel_board/firmware_vex/silicon_tests/common/recieve_packet.c
+++ caravel_board/firmware_vex/silicon_tests/common/recieve_packet.c
@@ -1,42 +1,34 @@
 #include "send_packet.c"
-
-
-void configure_mgmt_gpio_i(char en){
-    reg_gpio_ien = en;
-    reg_gpio_oe = en;
-    reg_gpio_out = 0; 
-}
-
 // used to detect recieving packet 
-int recieve_packet(int timeout){
-    reg_gpio_oe = 0; // disable the output
-    reg_gpio_out = 0; 
-    int packet_size = 0;
-    bool is_full_pulse = false; // indecator for full pulse if half pulse is just sent it will be false
-    while (reg_gpio_in == 1);
-    while (reg_gpio_in == 0);
-    while (reg_gpio_in == 1);
-    while (reg_gpio_in == 0);
-    packet_size++; // First packet not expected to get end of transmission 
-    is_full_pulse == true;
-    int count = 0;
+
+void configure_mgmt_gpio_input(){
+    reg_gpio_mode1 = 1;
+    reg_gpio_mode0 = 0; // Fixed for full swing operation
+    reg_gpio_ien = 1;
+    reg_gpio_oe = 0;
+    reg_gpio_out = 0; // default
+    count_down(PULSE_WIDTH*20);
+}
+// used to detect recieving packet 
+int recieve_packet(){
+    configure_mgmt_gpio_input();
+    int ones = 0;
+    int packet_size =1;
+    while (reg_gpio_in == 0); // busy wait 0
+    count_down(PULSE_WIDTH/2);
     while (true){
         if (reg_gpio_in == 1){
-            count++;
-            if (count >= timeout){
-                reg_gpio_oe = 1;// enable the output
-                reg_gpio_out = 1;
-                return packet_size;
-            }
+            ones++;
         }
         else if (reg_gpio_in == 0){
-            if (count > 0){ // indicate goes to 1 then 0 
-                is_full_pulse = !is_full_pulse;
-                if (is_full_pulse)
-                    packet_size++; 
-            }
-            count = 0;
+            if (ones > 0)
+                packet_size++;
+            ones = 0;
         }
+        if (ones>3){
+            configure_mgmt_gpio();
+            return packet_size;
+        }
+        count_down(PULSE_WIDTH);
     }
 }
-

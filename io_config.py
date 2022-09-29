e1918dc06e56096e9203547ef1f0dfc19bcccefc
Marwan Abbas <marwaneltoukhy@aucegypt.edu>
2022-09-29 12:57:17 +0200

    removed csv file generation

--- io_config.py
+++ io_config.py
@@ -354,7 +354,7 @@ def run_test_h(test, gpio_l, gpio_h):
     return True, None
 
 
-def change_config(channel, gpio_l, gpio_h, part, voltage, start_time):
+def change_config(channel, gpio_l, gpio_h, part, voltage, start_time, test):
     end_time = (time.time() - start_time) / 60.0
     if channel > 18:
         if gpio_h.get_io_stuck(37 - channel) == False:
@@ -375,20 +375,8 @@ def change_config(channel, gpio_l, gpio_h, part, voltage, start_time):
                     "minutes",
                 )
                 f = open(f"{part}.txt", "a")
-                fc = open(f"{part}_{test.voltage}.csv", "a")
                 f.write(f"\n\nPart: {part}\n")
-                fc.write(" \n")
-                fc.write(f"{test.voltage}\n")
                 arr_h = gpio_h.array[::-1]
-                for i in range(len(arr_h)):
-                    if i >= (37 - channel):
-                        fc.write("F\n")
-                    elif arr_h[i] == "H_NONE":
-                        fc.write("0\n")
-                    elif arr_h[i] == "H_DEPENDENT":
-                        fc.write("1\n")
-                    elif arr_h[i] == "H_INDEPENDENT":
-                        fc.write("2\n")
                 f.write(f"voltage: {voltage}\n")
                 for i in range(len(gpio_l.stuck)):
                     if gpio_h.get_io_stuck(i) == True:
@@ -421,19 +409,7 @@ def change_config(channel, gpio_l, gpio_h, part, voltage, start_time):
                     "minutes",
                 )
                 f = open(f"{part}.txt", "a")
-                fc = open(f"{part}_{test.voltage}.csv", "a")
                 f.write(f"\n\nPart: {part}\n")
-                fc.write(" \n")
-                fc.write(f"{test.voltage}\n")
-                for i in range(len(gpio_l.array)):
-                    if i >= channel:
-                        fc.write("F\n")
-                    elif gpio_l.array[i] == "H_NONE":
-                        fc.write("0\n")
-                    elif gpio_l.array[i] == "H_DEPENDENT":
-                        fc.write("1\n")
-                    elif gpio_l.array[i] == "H_INDEPENDENT":
-                        fc.write("2\n")
                 f.write(f"voltage: {voltage}\n")
                 for i in range(len(gpio_l.stuck)):
                     if gpio_l.get_io_stuck(i) == True:
@@ -489,7 +465,7 @@ def choose_test(
         else:
             run_flash(True)
             gpio_l, gpio_h = change_config(
-                channel_failed, gpio_l, gpio_h, part, test.voltage, start_time
+                channel_failed, gpio_l, gpio_h, part, test.voltage, start_time, test
             )
         if gpio_h.get_gpio_failed() is True or gpio_l.get_gpio_failed() is True:
             run_flash(True)
@@ -502,11 +478,7 @@ def test_passed(test, start_time, part, gpio_l, gpio_h, chain):
     print("Configuring the ios took: ", end_time, "minutes")
 
     f = open(f"{part}.txt", "a")
-    fc = open(f"{part}_{test.voltage}.csv", "a")
     f.write(f"\n\nPart: {part}\n")
-    fc.write(" \n")
-    fc.write(f"{test.voltage}\n")
-    fc.write(f"{part}\n")
     f.write(f"voltage: {test.voltage}\n")
     f.write(f"configuration of {chain} chain was successful\n")
     for i in range(len(gpio_l.stuck)):
@@ -518,26 +490,11 @@ def test_passed(test, start_time, part, gpio_l, gpio_h, chain):
     f.write(f"Final configuration of {chain} chain: \n")
     if chain == "low":
         f.write(f"gpio from 0 to 18: {gpio_l.array}\n")
-        for i in gpio_l.array:
-            if i == "H_NONE":
-                fc.write("0\n")
-            elif i == "H_DEPENDENT":
-                fc.write("1\n")
-            elif i == "H_INDEPENDENT":
-                fc.write("2\n")
     elif chain == "high":
         f.write(f"gpio from 37 to 19: {gpio_h.array}\n")
         arr = gpio_h.array[::-1]
-        for i in arr:
-            if i == "H_NONE":
-                fc.write("0\n")
-            elif i == "H_DEPENDENT":
-                fc.write("1\n")
-            elif i == "H_INDEPENDENT":
-                fc.write("2\n")
     f.write(f"Execution time: {end_time} minutes\n")
     f.close()
-    fc.close()
 
 
 if __name__ == "__main__":
@@ -635,9 +592,7 @@ if __name__ == "__main__":
 
         if os.path.exists(f"./{part}.txt"):
             os.remove(f"./{part}.txt")
-        if os.path.exists(f"./{part}_{test.voltage}.csv"):
-            os.remove(f"./{part}_{test.voltage}.csv")
-
+            
         if args.voltage:
             test.voltage = float(args.voltage)
 

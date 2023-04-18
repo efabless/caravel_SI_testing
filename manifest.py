9066c0cbbe433869c3db35b67b8134b4441e3d61
marwaneltoukhy <marwaneltoukhy@aucegypt.edu>
2023-04-18 19:59:01 +0200

    added memory checks in manifest file

--- manifest.py
+++ manifest.py
@@ -34,54 +34,54 @@ TestDict = [
             8,
         ],  # Passing criteria to be sent to mgmt_gpio
     },
-    # {
-    #     "test_name": "mem_sram_W",
-    #     "uart": False,
-    #     "mem": True,
-    #     "voltage": [1.6],
-    #     "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_W/mem_sram_W.hex",
-    #     "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
-    # },
-    # {
-    #     "test_name": "mem_sram_test",
-    #     "uart": False,
-    #     "mem": True,
-    #     "voltage": [1.6],
-    #     "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_test/mem_sram_test.hex",
-    #     "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
-    # },
-    # {
-    #     "test_name": "mem_sram_halfW",
-    #     "uart": False,
-    #     "mem": True,
-    #     "voltage": [1.6],
-    #     "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_halfW/mem_sram_halfW.hex",
-    #     "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
-    # },
-    # {
-    #     "test_name": "mem_dff_W",
-    #     "uart": False,
-    #     "mem": True,
-    #     "voltage": [1.6],
-    #     "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_W/mem_dff_W.hex",
-    #     "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
-    # },
-    # {
-    #     "test_name": "mem_dff_test",
-    #     "uart": False,
-    #     "mem": True,
-    #     "voltage": [1.6],
-    #     "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_test/mem_dff_test.hex",
-    #     "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
-    # },
-    # {
-    #     "test_name": "mem_dff_halfW",
-    #     "uart": False,
-    #     "mem": True,
-    #     "voltage": [1.6],
-    #     "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_halfW/mem_dff_halfW.hex",
-    #     "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
-    # },
+    {
+        "test_name": "mem_sram_W",
+        "uart": False,
+        "mem": True,
+        "voltage": [1.6],
+        "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_W/mem_sram_W.hex",
+        "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
+    },
+    {
+        "test_name": "mem_sram_test",
+        "uart": False,
+        "mem": True,
+        "voltage": [1.6],
+        "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_test/mem_sram_test.hex",
+        "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
+    },
+    {
+        "test_name": "mem_sram_halfW",
+        "uart": False,
+        "mem": True,
+        "voltage": [1.6],
+        "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_halfW/mem_sram_halfW.hex",
+        "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
+    },
+    {
+        "test_name": "mem_dff_W",
+        "uart": False,
+        "mem": True,
+        "voltage": [1.6],
+        "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_W/mem_dff_W.hex",
+        "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
+    },
+    {
+        "test_name": "mem_dff_test",
+        "uart": False,
+        "mem": True,
+        "voltage": [1.6],
+        "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_test/mem_dff_test.hex",
+        "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
+    },
+    {
+        "test_name": "mem_dff_halfW",
+        "uart": False,
+        "mem": True,
+        "voltage": [1.6],
+        "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_halfW/mem_dff_halfW.hex",
+        "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
+    },
     {
         "test_name": "cpu_stress",
         "uart": False,

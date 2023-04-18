fd95677bb22e698fa4486786761446c302eeeedf
marwaneltoukhy <marwaneltoukhy@aucegypt.edu>
2023-04-18 20:11:05 +0200

    fixed comments in manifest file

--- manifest.py
+++ manifest.py
@@ -9,18 +9,18 @@ device2_sn = (
 TestDict = [
     {
         "test_name": "uart",
-        "uart": True,
-        "mem": False,
+        "uart": True,  # Testing UART
+        "mem": False,  # NOT Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/uart/uart.hex",
         "passing_criteria": [
             "Monitor: Test UART passed"
-        ],  # Passing criteria to be sent to mgmt_gpio
+        ],  # Passing criteria to be sent to UART
     },
     {
         "test_name": "send_packet",
-        "uart": False,
-        "mem": False,
+        "uart": False,  # NOT Testing UART
+        "mem": False,  # NOT Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/send_packet/send_packet.hex",
         "passing_criteria": [
@@ -36,56 +36,56 @@ TestDict = [
     },
     {
         "test_name": "mem_sram_W",
-        "uart": False,
-        "mem": True,
+        "uart": False,  # NOT Testing UART
+        "mem": True,  # Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_W/mem_sram_W.hex",
         "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
     },
     {
         "test_name": "mem_sram_test",
-        "uart": False,
-        "mem": True,
+        "uart": False,  # NOT Testing UART
+        "mem": True,  # Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_test/mem_sram_test.hex",
         "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
     },
     {
         "test_name": "mem_sram_halfW",
-        "uart": False,
-        "mem": True,
+        "uart": False,  # NOT Testing UART
+        "mem": True,  # Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_sram_halfW/mem_sram_halfW.hex",
         "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
     },
     {
         "test_name": "mem_dff_W",
-        "uart": False,
-        "mem": True,
+        "uart": False,  # NOT Testing UART
+        "mem": True,  # Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_W/mem_dff_W.hex",
         "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
     },
     {
         "test_name": "mem_dff_test",
-        "uart": False,
-        "mem": True,
+        "uart": False,  # NOT Testing UART
+        "mem": True,  # Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_test/mem_dff_test.hex",
         "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
     },
     {
         "test_name": "mem_dff_halfW",
-        "uart": False,
-        "mem": True,
+        "uart": False,  # NOT Testing UART
+        "mem": True,  # Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/mem_dff_halfW/mem_dff_halfW.hex",
         "passing_criteria": [1, 3, 3, 3],  # Passing criteria to be sent to mgmt_gpio
     },
     {
         "test_name": "cpu_stress",
-        "uart": False,
-        "mem": False,
+        "uart": False,  # NOT Testing UART
+        "mem": False,  # NOT Testing mem
         "voltage": [1.6],
         "hex_file_path": f"{os.path.dirname(os.path.realpath(__file__))}/caravel_board/firmware_vex/mpw8_tests/cpu_stress/cpu_stress.hex",
         "passing_criteria": [

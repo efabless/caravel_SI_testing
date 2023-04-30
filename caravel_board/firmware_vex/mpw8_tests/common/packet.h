08cac0d9da7eeb0898abb212de84839f2e3052f4
M0stafaRady <mostafa.rady@efabless.com>
2023-04-30 04:54:52 -0700

    Add receive_packet test C code

--- caravel_board/firmware_vex/mpw8_tests/common/packet.h
+++ caravel_board/firmware_vex/mpw8_tests/common/packet.h
@@ -70,4 +70,36 @@ void configure_mgmt_gpio(){
     count_down(PULSE_WIDTH*20);
 }
 
+
+void configure_mgmt_gpio_input(){
+    reg_gpio_mode1 = 1;
+    reg_gpio_mode0 = 0; // Fixed for full swing operation
+    reg_gpio_ien = 1;
+    reg_gpio_oe = 0;
+    reg_gpio_out = 0; // default
+    count_down(PULSE_WIDTH*20);
+}
+
+int receive_packet(){
+    int ones = 0;
+    int packet_size =1;
+    while (reg_gpio_in == 0); // busy wait 0
+    count_down(PULSE_WIDTH/2);
+    while (true){
+        if (reg_gpio_in == 1){
+            ones++;
+        }
+        else if (reg_gpio_in == 0){
+            if (ones > 0)
+                packet_size++;
+            ones = 0;
+        }
+        if (ones>3){
+            configure_mgmt_gpio();
+            return packet_size;
+        }
+        count_down(PULSE_WIDTH);
+    }
+}
+
 #endif // PACKET_C_HEADER_FILE
---
+++ caravel_board/firmware_vex/mpw8_tests/receive_packet/receive_packet.c
@@ -0,0 +1,32 @@
+#include <common.h>
+/*
+test is using uart as a comunication channel 
+
+/*
+@ send "ready" through uart
+    send packets of n through mgmt_gpio 
+
+loop 
+    @ send "number of = n" through uart 
+        send packets of n through mgmt_gpio
+    
+
+*/
+
+
+
+void main(){
+    int recieved_size = 0;
+    configure_mgmt_gpio_input();
+    configure_gpio(6,GPIO_MODE_MGMT_STD_OUTPUT);
+    // gpio_config_io();
+    gpio_config_load();
+    print("ready"); 
+    while(true){
+        recieved_size = receive_packet(); 
+        print("number of = ");
+        print(recieved_size);
+    }
+    
+
+}

f0e7c6da18483df7c664773673e284ed03f5964b
jeffdi <jeffdi@efabless.com>
2023-04-30 07:18:44 -0700

    fix uart

--- caravel.py
+++ caravel.py
@@ -488,8 +488,9 @@ class UART:
 
     def close(self):
 
-        dwf.FDwfDeviceClose(self.device_data.handle)
-        device.open(self.device_data.handle)
+        # dwf.FDwfDeviceClose(self.device_data.handle)
+        # device.open(self.device_data.handle)
+        dwf.FDwfDigitalUartReset(self.device_data.handle)
 
 
 class SPI:

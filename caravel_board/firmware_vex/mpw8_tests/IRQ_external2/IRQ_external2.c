f4740b3f66501d5a5175841c6d099f1cfb09d78a
M0stafaRady <mostafa.rady@efabless.com>
2023-04-30 04:54:27 -0700

    Add IRQ_external2 test  C code

---
+++ caravel_board/firmware_vex/mpw8_tests/IRQ_external2/IRQ_external2.c
@@ -0,0 +1,78 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Efabless Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <common.h>
+
+
+/*
+Testing IRQ interrupts 
+Enable interrupt for IRQ external pin mprj_io[12] -> should be drived to 1 by the environment
+**NOTE** housekeeping SPI should used to update register irq_1_inputsrc to 1 see verilog code
+
+    @wait for environment to make mprj[12] high
+        send packet size = 1
+
+    @received interrupt correctly  test pass
+        send packet size = 5
+
+    @ timeout                       test fail
+        send packet size = 9
+
+    @ end test 
+        send packet size = 3
+        send packet size = 3
+        send packet size = 3
+
+*/
+
+
+void main(){
+    configure_mgmt_gpio();
+
+    // setting bit 12 as input
+    configure_gpio(12, GPIO_MODE_MGMT_STD_INPUT_NOPULL);
+    // gpio_config_io();
+
+    // automatic bitbang approach
+    gpio_config_load();
+
+    enable_external2_irq(1);
+    clear_flag();
+    send_packet(1);//wait for environment to make mprj[12] high 
+
+    // Loop, waiting for the interrupt to change reg_mprj_datah
+    bool is_pass = false;
+    int timeout = 400000; 
+
+    for (int i = 0; i < timeout; i++){
+        if (get_flag() == 1){
+            send_packet(5);//test pass irq sent
+            is_pass = true;
+            break;
+        }
+    }
+    if (!is_pass){
+        send_packet(9);// timeout
+    }
+
+    // finish test
+    send_packet(3);
+    send_packet(3);
+    send_packet(3);
+
+}
+

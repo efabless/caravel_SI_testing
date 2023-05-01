f9416e4a3b868c05fd2142b43eb290cf164e4c04
marwaneltoukhy <marwaneltoukhy@aucegypt.edu>
2023-05-01 14:07:39 +0300

    fix hex receive_packet

--- caravel_board/firmware_vex/mpw8_tests/receive_packet/receive_packet.c
+++ caravel_board/firmware_vex/mpw8_tests/receive_packet/receive_packet.c
@@ -16,7 +16,21 @@ loop
 void main()
 {
     configure_mgmt_gpio();
-    configure_all_gpios(GPIO_MODE_MGMT_STD_OUTPUT);
+
+    // setting bit 7 as input
+    configure_gpio(7, GPIO_MODE_MGMT_STD_INPUT_NOPULL);
+    // gpio_config_io();
+
+    // automatic bitbang approach
+    gpio_config_load();
+
+    enable_external1_irq(1);
+    user_irq_1_ev_pending_write(1);
+    user_irq_0_ev_pending_write(1);
+    user_irq_2_ev_pending_write(1);
+    user_irq_3_ev_pending_write(1);
+    user_irq_4_ev_pending_write(1);
+    user_irq_5_ev_pending_write(1);
     send_packet(2);
     int recieved_size = 0;
     configure_mgmt_gpio_input();

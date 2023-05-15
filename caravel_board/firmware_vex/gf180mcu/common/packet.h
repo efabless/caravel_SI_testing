414249179126939cf85c4f949ca0dc02e6aa0564
jeffdi <jeffdi@efabless.com>
2023-05-15 10:48:37 -0700

    update mem_sram_W with upper and lower

--- caravel_board/firmware_vex/gf180mcu/common/packet.h
+++ caravel_board/firmware_vex/gf180mcu/common/packet.h
@@ -71,7 +71,7 @@ void configure_mgmt_gpio()
     reg_gpio_ien = 1;
     reg_gpio_oeb = 0;
     reg_gpio_out = 1; // default
-    count_down(PULSE_WIDTH * 70);
+    count_down(PULSE_WIDTH * 100);
 }
 
 void configure_mgmt_gpio_input()
@@ -81,7 +81,7 @@ void configure_mgmt_gpio_input()
     reg_gpio_ien = 1;
     reg_gpio_oeb = 1;
     reg_gpio_out = 0; // default
-    count_down(PULSE_WIDTH * 70);
+    count_down(PULSE_WIDTH * 100);
 }
 
 int receive_packet()

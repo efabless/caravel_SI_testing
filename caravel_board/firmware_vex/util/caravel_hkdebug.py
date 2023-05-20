dbabd14eb28542ab2980919e3af6733b44d3a68f
jeffdi <jeffdi@efabless.com>
2023-05-19 17:50:19 -0700

    debug gpio_lpu_ho

--- caravel_board/firmware_vex/util/caravel_hkdebug.py
+++ caravel_board/firmware_vex/util/caravel_hkdebug.py
@@ -171,27 +171,7 @@ while k != "q":
     k = input()
 
     if k == "1":
-        for reg in [
-            0x00,
-            0x01,
-            0x02,
-            0x03,
-            0x04,
-            0x05,
-            0x06,
-            0x07,
-            0x08,
-            0x09,
-            0x0A,
-            0x0B,
-            0x0C,
-            0x0D,
-            0x0E,
-            0x0F,
-            0x10,
-            0x11,
-            0x12,
-        ]:
+        for reg in range(0x00, 0x70):
             data = slave.exchange([CARAVEL_REG_READ, reg], 1)
             print("reg {} = {}".format(hex(reg), binascii.hexlify(data)))
 

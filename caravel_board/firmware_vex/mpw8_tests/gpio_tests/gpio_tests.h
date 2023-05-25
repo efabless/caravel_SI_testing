339222b7bd2c157d2a705997b38a1c88f285e71e
marwaneltoukhy <marwaneltoukhy@aucegypt.edu>
2023-05-25 12:07:59 +0300

    added gpio_tests

---
+++ caravel_board/firmware_vex/mpw8_tests/gpio_tests/gpio_tests.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+#include "../gpio_o/gpio_o.c"
+#include "../gpio_i/gpio_i.c"
+#include "../bitbang_i/bitbang_i.c"
+#include "../bitbang_o/bitbang_o.c"
+
+#endif // HEADER_FILE_H

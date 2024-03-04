ce53d4b3359c2961f9d5cce0a05d04401b022bfc
marwaneltoukhy <marwaneltoukhy@aucegypt.edu>
2024-03-04 11:14:39 +0200

    added soc_test

--- caravel_board/firmware_vex/swift/soc_tests/soc_tests.c
+++ caravel_board/firmware_vex/swift/soc_tests/soc_tests.c
@@ -3,137 +3,143 @@
 
 void main()
 {
-    config_uart();
-    print("Start Test: cpu_stress\n");
-    count_down(PULSE_WIDTH*10);
-    if (cpu_stress())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    config_uart();
-    print("Start Test: hk_regs_wr_wb_cpu\n");
-    // count_down(PULSE_WIDTH*10);
-    if (hk_regs_wr_wb_cpu())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    if (IRQ_external())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    if (IRQ_external2())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    config_uart();
-    print("Start Test: IRQ_spi\n");
-    // count_down(PULSE_WIDTH*10);
-    if (IRQ_spi())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    config_uart();
-    print("Start Test: IRQ_timer\n");
-    // count_down(PULSE_WIDTH*10);
-    if (IRQ_timer())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    config_uart();
-    print("Start Test: IRQ_uart\n");
-    // count_down(PULSE_WIDTH*10);
-    if (IRQ_uart())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    config_uart();
-    print("Start Test: timer0_oneshot\n");
-    // count_down(PULSE_WIDTH*10);
-    if (timer0_oneshot())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    config_uart();
-    print("Start Test: timer0_periodic\n");
-    // count_down(PULSE_WIDTH*10);
-    if (timer0_periodic())
-    {
-        config_uart();
-        print("passed\n");
-    }
-    else
-    {
-        config_uart();
-        print("failed\n");
-    }
-
-    // if (IRQ_uart_rx())
+    // HKGpio_config();
+    // while (1)
     // {
-    //     config_uart();
-    //     print("passed\n");
-    // }
-    // else
-    // {
-    //     config_uart();
-    //     print("failed\n");
-    // }
-    config_uart();
-    print("End Test\n");
+    //     HKGpio_config();
+    ManagmentGpio_inputEnable();
+    if (reg_gpio_in == 0){
+        config_uart_ios();
+        config_uart();
+        print("ST: cpu_stress\n");
+        if (cpu_stress())
+        {
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {
+            config_uart();
+            print("failed\n");
+        }
+
+        config_uart();
+        print("ST: hk_regs_wr_wb_cpu\n");
+        if (hk_regs_wr_wb_cpu())
+        {
+            config_uart_ios();
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {   
+            config_uart_ios();
+            config_uart();
+            print("failed\n");
+        }
+
+        if (IRQ_external())
+        {
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {
+            config_uart();
+            print("failed\n");
+        }
+
+        if (IRQ_external2())
+        {
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {
+            config_uart();
+            print("failed\n");
+        }
+
+        config_uart();
+        print("ST: IRQ_spi\n");
+        if (IRQ_spi())
+        {
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {
+            config_uart();
+            print("failed\n");
+        }
+
+        config_uart();
+        print("ST: IRQ_timer\n");
+        if (IRQ_timer())
+        {
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {
+            config_uart();
+            print("failed\n");
+        }
+
+        config_uart();
+        print("ST: IRQ_uart\n");
+        if (IRQ_uart())
+        {
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {
+            config_uart();
+            print("failed\n");
+        }
+
+        config_uart();
+        print("ST: timer0_oneshot\n");
+        if (timer0_oneshot())
+        {
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {
+            config_uart();
+            print("failed\n");
+        }
+
+        config_uart();
+        print("ST: timer0_periodic\n");
+        if (timer0_periodic())
+        {
+            config_uart();
+            print("passed\n");
+        }
+        else
+        {
+            config_uart();
+            print("failed\n");
+        }
+
+        // if (IRQ_uart_rx())
+        // {
+        //     config_uart();
+        //     print("passed\n");
+        // }
+        // else
+        // {
+        //     config_uart();
+        //     print("failed\n");
+        // }
+        // HKGpio_config();
+        config_uart();
+        print("End Test\n");
+    }
+    // };
+
     HKGpio_config();
 }

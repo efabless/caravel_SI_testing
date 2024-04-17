b2f4e024d8fcbd1911138dc83f4da913d20ec1cb
Passant <bassant.hassan@efabless.com>
2024-04-17 16:51:09 +0200

    [wip] working method to configure FPGA through Caravel IOs

--- silicon_tests/clear/inv_small/inv_small.c
+++ silicon_tests/clear/inv_small/inv_small.c
@@ -34,45 +34,10 @@ void delay(int delayTime) {
 // }
 
 void process_bit_stream() {
-    //Blizzard
-    // reg_mprj_datal &= 0x0;
-    // reg_mprj_datal = 0x100;
-    // delay(2);
-    // for (unsigned int i = 0; i < inv_small_size; i++)
-    // {
-    //     int xdata = inv_small[i];
-    //     // int xdata =0x0F;
-    //     unsigned int io_data = 0;
-    //     for (int j = 0; j < 8; j++) {
-    //         io_data = (xdata & 0x1) ? 0x2100 : 0x100;
-    //         // | 0x280 : io_data & 0xFFFFFDFF;
-    //         reg_mprj_datal = io_data;
-    //         // toggle prog clk
-    //         io_data = (xdata & 0x1) ? 0x2180 : 0x180;
-    //         reg_mprj_datal = io_data;
-    //         // prog clk 0
-    //         io_data = (xdata & 0x1) ? 0x2100 : 0x100;
-    //         reg_mprj_datal = io_data;
-    //         // reg_mprj_datal &= clk_zero_mask;
-    //         // reg_mprj_datal |= clk_one_mask;
-    //         xdata = xdata >> 1;
-    //     }
-    // }
-    // // prog clk 0
-    // reg_mprj_datal &= 0x2100;
-    // reg_mprj_datal |= 0x1100;
-    // delay(2);
-    // reg_mprj_datal &= 0x1100;
-    // reg_mprj_datal |= 0x5100;
-    // reg_mprj_datal |= 0x5300;
-    // delay(2);
 
-    //Clear
     reg_mprj_datal = 0x0;
-    delay(1);
     reg_mprj_datal = 0x100;
-    delay(2);
-    unsigned int io_data = 0;
+    unsigned int io_data;
     for (unsigned int i = 0; i < and_3_size; i++)
     {
         int xdata = and_3[i];
@@ -82,25 +47,33 @@ void process_bit_stream() {
             io_data = (xdata & 0x1) ? 0x2100 : 0x100;
             // | 0x280 : io_data & 0xFFFFFDFF;
             reg_mprj_datal = io_data;
+            // delay(10);
             // toggle prog clk
             io_data = (xdata & 0x1) ? 0x2180 : 0x180;
             reg_mprj_datal = io_data;
+            // delay(10);
             // prog clk 0
             io_data = (xdata & 0x1) ? 0x2100 : 0x100;
             reg_mprj_datal = io_data;
+            // delay(10);
             // reg_mprj_datal &= clk_zero_mask;
             // reg_mprj_datal |= clk_one_mask;
             xdata = xdata >> 1;
+            // delay(10);
         }
     }
     // prog clk 0
     reg_mprj_datal = io_data & 0x2100;
     reg_mprj_datal = io_data | 0x1100;
-    delay(2);
+    // delay(10);
     reg_mprj_datal = io_data & 0x1100;
+    // delay(10);
     reg_mprj_datal = io_data | 0x5100;
-    reg_mprj_datal = io_data | 0x5980;
-    delay(2);
+    // delay(10);
+    //Blizzard
+    reg_mprj_datal = io_data | 0x5300;
+    //Clear
+    // reg_mprj_datal = io_data | 0x5900;
 }
 
 void main()
@@ -112,7 +85,10 @@ void main()
     // if (reg_gpio_in == 0) {
     // FPGA IOs
     configure_gpio(1, GPIO_MODE_USER_STD_INPUT_NOPULL);
+    //Clear
     configure_gpio(9, GPIO_MODE_USER_STD_INPUT_NOPULL);
+    //Blizzard
+    // configure_gpio(11, GPIO_MODE_USER_STD_INPUT_NOPULL);
     configure_gpio(23, GPIO_MODE_USER_STD_OUTPUT);
     configure_gpio(29, GPIO_MODE_USER_STD_INPUT_NOPULL);
     configure_gpio(34, GPIO_MODE_USER_STD_INPUT_NOPULL);
@@ -122,7 +98,10 @@ void main()
     // ==
     configure_gpio(7, GPIO_MODE_MGMT_STD_OUTPUT); // connected to prog_clk
     configure_gpio(8, GPIO_MODE_MGMT_STD_OUTPUT); // connected to prog_rst
+    //Clear
     configure_gpio(11, GPIO_MODE_MGMT_STD_OUTPUT); // connected to op_rst
+    //Blizzard
+    // configure_gpio(9, GPIO_MODE_MGMT_STD_OUTPUT); // connected to op_rst
     configure_gpio(12, GPIO_MODE_MGMT_STD_OUTPUT); // connected to isol_n
     configure_gpio(13, GPIO_MODE_MGMT_STD_OUTPUT); // connected to ccff_head
     configure_gpio(14, GPIO_MODE_MGMT_STD_OUTPUT); // connected to clk_sel
